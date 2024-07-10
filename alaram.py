8a2e15a7d21b44b98c63ae9402d25c3a448c0674
SUMANTHRAJUODINSON <128466176+SUMANTHRAJUODINSON@users.noreply.github.com>
2024-07-10 15:26:23 +0530

    Update alaram.py

--- alaram.py
+++ alaram.py
@@ -6,7 +6,6 @@ import pyttsx3
 from threading import Thread
 
 # Initialize text-to-speech engine
-
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)  # Speed percent (can go over 100)
 engine.setProperty('volume', 0.9)  # Volume 0-1
